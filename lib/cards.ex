c85a588be073ea4fe44c529ffda37fac78e5c8c0
kamalogudah <mcpaul2058@gmail.com>
2017-01-11 14:41:04 +0300

    Added a README

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,11 @@
 defmodule Cards do
     def create_deck do
-        ["Ace","Two", "Three"]
+        values = ["Ace","Two", "Three", "Four","Five"]
+        suits = ["Spades", "CLubs","Hearts", "Diamonds"]
+
+        for suit <- suits do
+            suit
+        end
     end
     def shuffle(deck) do
         Enum.shuffle(deck)
