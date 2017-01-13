3018893ec2cc4b1ee4d2c7c06c6c1e184630c327
kamalogudah <mcpaul2058@gmail.com>
2017-01-13 15:59:52 +0300

    Added Documentation and the game finally works

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,11 +1,20 @@
 defmodule Cards do
+
+    @moduledoc """
+       Provides methods for shuffling a dec of cards
+    """
+
+    @doc """
+      Returns a list of strings representing a deck of playing cards
+      """
     def create_deck do
         values = ["Ace","Two", "Three", "Four","Five"]
         suits = ["Spades", "CLubs","Hearts", "Diamonds"]
 
-        for suit <- suits do
-            suit
+        for suit <- suits, value <- values do
+            "#{value} of #{suit}"
         end
+        
     end
     def shuffle(deck) do
         Enum.shuffle(deck)
@@ -14,4 +23,54 @@ defmodule Cards do
         Enum.member?(deck, card)
         
     end
+
+    @doc """
+      Divides a deck into a hand and the remainder of the deck. 
+      The `hand_size` argument indicates how many cards should be in the hand.
+    
+    ## Examples
+
+          iex> deck = Cards.create_deck
+          iex> { hand, deck} = Cards.deal(deck, 1)
+          iex> hand
+          ["Ace of Spades"]
+              
+    """   
+    
+    def deal(deck, hand_size) do
+        Enum.split(deck, hand_size)
+        
+    end
+
+    def save(deck, filename) do
+        binary =:erlang.term_to_binary(deck)
+        File.write(filename, binary)
+
+    end 
+
+    def load(filename) do
+        {status, binary} = File.read(filename)
+
+        case status do 
+            :ok -> :erlang.binary_to_term binary
+            :error -> "That file does not exist"
+        end 
+        #shorthand of the above method
+        #case File.read(filename) do
+         #   {:ok, binary} -> :erlang.binary_to_term binary
+          #  {:error, _reason} -> "That file does not exist"
+
+        #end
+    end
+
+    def create_hand(hand_size) do
+        #deck = Cards.create_deck
+        #deck = Cards.shuffle(deck)
+        #hand = Cards.deal(deck, hand_size)
+        #Doing the above in a better way 
+        Cards.create_deck
+        |> Cards.shuffle
+        |> Cards.deal(hand_size)
+    end
 end
+  
